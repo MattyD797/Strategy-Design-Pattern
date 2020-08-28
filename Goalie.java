002ba2aba1402a15f59bf84a61088da777f645fd
mtduggan <69335573+mtduggan@users.noreply.github.com>
2020-08-28 12:58:33 -0400

    Delete Goalie.java

--- Goalie.java
+++
@@ -1,9 +0,0 @@
-public class Goalie extends Player {
-  public Goalie(String name){
-    super(name);
-    defenceBehavior = offenceBehavior = new BlockGoalBehavior();
-  }
-  public void toString(){
-    
-  }
-}
